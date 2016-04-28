888291e70887034fe35fd6d675e62ae54ed2fdad
liubaicai <945725069@qq.com>
2016-04-29 00:46:45 +0800

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,20 @@
 class ArticlesController < ApplicationController
+
   def index
+    @articles = Article.order('created_at DESC').page(params[:page])
   end
+
+  def new
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to action: :index
+  end
+  
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+  
 end
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get cteate" do
+    get :cteate
     assert_response :success
   end
 
