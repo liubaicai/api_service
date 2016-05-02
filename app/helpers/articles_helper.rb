e92639aefaeee8b5ddc33741fc3ac793fb41a452
liubaicai <945725069@qq.com>
2016-05-02 22:03:24 +0800

    update

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,11 @@
 module ArticlesHelper
+    
+    def selectdefault category_id,current_id
+        if category_id==current_id
+            'selected="selected"'
+        else
+            ''
+        end
+    end
+
 end
