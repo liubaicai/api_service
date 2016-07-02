8164ff3082138a0dec96d964881a81913257df24
刘白菜 <liushuai.baicai@hotmail.com>
2016-07-02 11:01:45 +0800

    add views count

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,8 @@ class ArticlesController < ApplicationController
   
   def show
     @article = Article.find(params[:id])
+    @article.views = @article.views+1;
+    @article.save
   end
 
   def new
--- db/structure.sql
+++ db/structure.sql
@@ -2,7 +2,7 @@ CREATE TABLE "schema_migrations" ("version" varchar NOT NULL);
 CREATE UNIQUE INDEX "unique_schema_migrations" ON "schema_migrations" ("version");
 CREATE TABLE "categories" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
 CREATE TABLE "links" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "title" varchar, "url" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL, "sort" integer);
-CREATE TABLE "articles" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "title" varchar, "text" text(16777214), "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL, "category_id" integer);
+CREATE TABLE "articles" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "title" varchar, "text" text(16777214), "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL, "category_id" integer, "views" integer DEFAULT 0);
 CREATE INDEX "index_articles_on_category_id" ON "articles" ("category_id");
 CREATE TABLE "configs" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "sc_key" varchar, "sc_value" text, "sc_note" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
 CREATE TABLE "req_logs" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "ip" varchar, "url" varchar(250), "method" varchar, "ua" varchar(250), "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
@@ -23,4 +23,6 @@ INSERT INTO schema_migrations (version) VALUES ('20160502112707');
 INSERT INTO schema_migrations (version) VALUES ('20160504151342');
 
 INSERT INTO schema_migrations (version) VALUES ('20160508071635');
+
+INSERT INTO schema_migrations (version) VALUES ('20160702024312');
 
