f33f1ad7815fac6d4f8b877b93a33647b1d457af
Baicai <liushuai.baicai@hotmail.com>
2017-09-02 22:44:34 +0800

    update error info

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -8,7 +8,7 @@ class ConfigsController < ApplicationController
       model = Model.new(200,'success',Hash[:token => Digest::MD5.hexdigest("#{Config.getValue('pwd')}#{Time.now.month}")])
       render :json =>model
     else
-      model = Model.new(300,'error','密码有误。')
+      model = Model.new(300,'密码有误。','')
       render :json =>model
     end
   end
